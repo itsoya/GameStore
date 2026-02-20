0fd64e4483e9a12b64123402aa14ea8ce4028b98
Itsoya Abebe <itsoyaa@gmail.com>
2026-02-20 09:43:58 -0500

    .gitignore is now working

--- GameStore/Dtos/CreateGameDto.cs
+++
@@ -1,8 +0,0 @@
-namespace GameStore.Dtos;
-
-public record CreateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
-    DateOnly ReleaseDate
-);
--- GameStore/Dtos/UpdateGameDto.cs
+++
@@ -1,9 +0,0 @@
-namespace GameStore.Dtos;
-
-public record UpdateGameDto
-(
-    string Name,
-    string Genre,
-    decimal Price,
-    DateOnly ReleaseDate
-);
